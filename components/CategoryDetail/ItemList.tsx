e00c4ae7fa6643ac7fb5528f05d98c5bce4e5455
KelvinCYDev <kelvincy.dev@gmail.com>
2025-03-15 18:49:23 -0700

    Update: Delete item button

--- components/CategoryDetail/ItemList.tsx
+++ components/CategoryDetail/ItemList.tsx
@@ -1,13 +1,32 @@
 import { supabase } from "@/utils/SupabaseConfig";
+import { EvilIcons } from "@expo/vector-icons";
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
+import Toast from "react-native-simple-toast";
 
 export default function ItemList({
   categoryData,
+  getCategoryDetail,
 }: {
   categoryData: categoryDataType;
+  getCategoryDetail: () => Promise<void>;
 }) {
   const [explandItem, setExpandItem] = useState(0);
+
+  const onDeleteItem = async (id: string) => {
+    const { error } = await supabase
+      .from("CategoryItems")
+      .delete()
+      .eq("id", id);
+    Toast.show("Item Deleted!", Toast.SHORT);
+    getCategoryDetail();
+  };
+
+  const openURL = (url: string) => {
+    if (url) {
+      Linking.openURL(url);
+    }
+  };
   return (
     <View className="mt-10">
       <Text className="font-[outfit-bold] text-xl">Item List</Text>
@@ -34,16 +53,16 @@ export default function ItemList({
                 </View>
                 <Text className="text-lg font-[outfit-bold]">${item.cost}</Text>
               </TouchableOpacity>
-              {/* {explandItem==index&&
-            <View style={styles.actionItemContainer}>
-                <TouchableOpacity onPress={()=>onDeleteItem(item.id)}>
+              {explandItem == index && (
+                <View className="flex flex-row gap-3 justify-end">
+                  <TouchableOpacity onPress={() => onDeleteItem(item.id)}>
                     <EvilIcons name="trash" size={34} color="red" />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={()=>openURL(item.url)}>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => openURL(item.url)}>
                     <EvilIcons name="external-link" size={34} color="blue" />
-                </TouchableOpacity>
-            </View>
-            } */}
+                  </TouchableOpacity>
+                </View>
+              )}
               {categoryData?.CategoryItems.length - 1 != index && (
                 <View className="border mt-5 border-GRAY"></View>
               )}
